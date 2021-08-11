81be155f1850ebddb91789643c60af4a91ba25bf
Gaya Rabello <gayarabello@gmail.com>
2021-08-11 17:51:32 +0100

    Added inputs and cards - part 2

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,42 +1,85 @@
 <template>
-  <q-page class="row items-center justify-evenly"> Carmera </q-page>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        class="full-width"
+        src="https://images.unsplash.com/photo-1554792998-80c29ca41cfb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=916&q=80"
+        alt=""
+      />
+    </div>
+
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" icon="eva-camera" round size="lg" />
+
+      <div class="justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="caption"
+          dense
+        />
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="location"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn rounded dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="column justify-center q-mt-lg">
+        <q-btn
+          @click="publishPost()"
+          unelevated
+          rounded
+          color="primary"
+          icon="eva-navigation-2-outline"
+        >
+          Publish
+        </q-btn>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
+import { uid } from 'quasar';
+import { Meta } from 'components/models';
 /* import ExampleComponent from 'components/CompositionComponent.vue'; */
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
   components: {},
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now(),
+      },
+    };
+  },
+  methods: {
+    publishPost() {
+      console.log(this.post);
+    },
+  },
   setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1',
-      },
-      {
-        id: 2,
-        content: 'ct2',
-      },
-      {
-        id: 3,
-        content: 'ct3',
-      },
-      {
-        id: 4,
-        content: 'ct4',
-      },
-      {
-        id: 5,
-        content: 'ct5',
-      },
-    ]);
     const meta = ref<Meta>({
       totalCount: 1200,
     });
-    return { todos, meta };
+    return { meta };
   },
 });
 </script>
+<style lang="scss" scoped>
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,15 +1,98 @@
 <template>
-  <q-page class="row items-center justify-evenly"> Home </q-page>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-md-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img :src="profilePic" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Gaya Rabello</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator />
+          <q-img :src="post.imageUrl"></q-img>
+          <q-card-section>
+            <div class="text-h6">{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar>
+              <img :src="profilePic" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Gaya Rabello</q-item-label>
+            <q-item-label caption> @gayarabello </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
+import { date } from 'quasar';
 import { Meta } from 'components/models';
 /* import ExampleComponent from 'components/CompositionComponent.vue'; */
 import { defineComponent, ref } from 'vue';
-
+const profilePic =
+  'https://res.cloudinary.com/practicaldev/image/fetch/s--klBCXUfL--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/509970/e2ac2bfa-2d1d-42f8-b263-e9e0608a26ae.png';
 export default defineComponent({
   name: 'PageIndex',
   components: {},
+  data() {
+    return {
+      profilePic,
+      posts: [
+        {
+          id: 1,
+          caption: 'A beloved Bridge',
+          date: 1628697703,
+          location: 'Lisbon, Portugal',
+          imageUrl:
+            'https://images.unsplash.com/photo-1627679482560-9258e05a4226?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYyODY5NjQ5OQ&ixlib=rb-1.2.1&q=80&w=1080',
+        },
+        {
+          id: 2,
+          caption: 'Random Image',
+          date: 1628697703,
+          location: 'Valencia, Spain',
+          imageUrl:
+            'https://images.unsplash.com/photo-1544180447-45e0c7167b48?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80',
+        },
+        {
+          id: 3,
+          caption: 'Una image qualquier',
+          date: 1628697703,
+          location: 'Copenhagen, Denmark',
+          imageUrl:
+            'https://images.unsplash.com/photo-1554792998-80c29ca41cfb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=916&q=80',
+        },
+      ],
+    };
+  },
+  methods: {
+    niceDate(value: number): string {
+      return date.formatDate(value, 'MMMM D T h:mmA');
+    },
+  },
   setup() {
     const meta = ref<Meta>({
       totalCount: 1200,
@@ -18,3 +101,10 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="scss" scoped>
+.card-post {
+  .q-img {
+    min-height: 200px;
+  }
+}
+</style>
